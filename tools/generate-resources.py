f55fc1bf6f947bf9d8ea91a5082719cb36bd4209
Kazuya Takei <myself@attakei.net>
2022-08-19 21:52:46 +0900

    feat: Generator of package resources (PKGBUILD and SRCINFO)

---
+++ tools/generate-resources.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+"""Generate resources of package in AUR.
+"""
+import argparse
+import configparser
+import hashlib
+import sys
+from dataclasses import dataclass
+from pathlib import Path
+
+import httpx
+import jinja2
+
+
+class Arguments:
+    root: Path
+    dist: Path
+
+    def resolve(self):
+        self.root = (Path.cwd() / self.root).resolve()
+        if not self.root.exists():
+            raise ValueError("--root is not exists")
+        if not self.root.is_dir():
+            raise ValueError("--root is not directory")
+        self.dist = (Path.cwd() / self.dist).resolve()
+
+
+@dataclass
+class Source:
+    url: str
+    checksum: str
+
+    @classmethod
+    def load(cls, url: str) -> "Source":
+        resp = httpx.get(url, follow_redirects=True)
+        checksum = hashlib.md5(resp.content).hexdigest()
+        return cls(url=url, checksum=checksum)
+
+
+def main(args: Arguments) -> int:
+    cfg = configparser.ConfigParser()
+    cfg.read(args.root / "package.ini")
+    ctx = {
+        "version": cfg.get("main", "version"),
+        "sources": [],
+    }
+    ctx["version_text"] = ctx["version"][1:]
+    for name, url in cfg.items("sources"):
+        src = Source.load(url.format(**ctx))
+        ctx["sources"].append(src)
+    print(ctx)
+    for template in (args.root / "templates").glob("*.j2"):
+        t = jinja2.Template(template.read_text())
+        dist = args.dist / template.stem
+        dist.write_text(t.render(**ctx))
+    return 0
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--root", default=".")
+    parser.add_argument("dist")
+
+    try:
+        args = parser.parse_args(namespace=Arguments())
+        args.resolve()
+        ret = main(args)
+    except ValueError as err:
+        print(err)
+        ret = 1
+    sys.exit(ret)
